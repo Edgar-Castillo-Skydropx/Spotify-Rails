1fc5433d054dae41def2c91a06a1e9556e4d66bc
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2024-11-04 16:08:47 -0600

    feat: some fixes and improvements were added in the root project module [v1]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :artists
-  get "home/index"
+
+  resource :dashboard, to: "artists#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +11,10 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  authenticated :artist do
+    root "artists#dashboard", as: :authenticated_artist_root
+  end
 
   # Defines the root path route ("/")
   root "home#index"
