08766aea30401b38f0d9f12e97ae36f24be191cc
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2024-11-20 12:05:00 -0600

    feat: some features were implemented and fixes were added on all the modules [v1]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_151956) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_19_224551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_151956) do
     t.datetime "updated_at", null: false
     t.string "stage_name"
     t.string "stripe_account_id"
+    t.integer "stripe_status", default: 0
     t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
   end
@@ -74,6 +75,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_151956) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "webhook_events", force: :cascade do |t|
+    t.string "source"
+    t.text "data"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
